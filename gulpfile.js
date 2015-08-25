5b10dfc39432ad31d485d67ac0fbede4d089bce0
Rama Moorthy <ram.praximo@gmail.com>
2015-08-26 07:03:18 +0800

    bootsraping

--- gulpfile.js
+++ gulpfile.js
@@ -16,8 +16,7 @@ var gulp = require('gulp'),
     order = require('gulp-order'),
     Q = require('q'),
     rename = require('gulp-rename'),
-    replace = require('gulp-replace')
-    rsass = require('gulp-ruby-sass'),
+    replace = require('gulp-replace'),
     sourcemaps = require('gulp-sourcemaps'),
     streamQueue = require('streamqueue'),
     through = require('through'),
@@ -28,4 +27,43 @@ var config = {
   src: 'assets',
   build: 'public'
 };
+
+
+gulp.task('clean', function() {
+  return gulp.src(config.build)
+    .pipe(clean())
+});
+
+gulp.task('build-vendor-dev', ['clean'], function() {
+  return bowerFiles({env: 'development'})
+    .pipe(filter('!**/airbrake-shim.js'))
+    .pipe(gulp.dest(config.build + '/bower_components'));
+});
+
+gulp.task('build-html-dev', [
+  'clean',
+  'build-vendor-dev'
+], function() {
+  var sourceStream = streamQueue(
+    {objectMode: true},
+    bowerFiles({env: 'development'})
+      .pipe(filter('!**/airbrake-shim.js')));
+
+  return gulp.src(config.src + '/index.tpl.html')
+    .pipe(inject(sourceStream, {
+      addRootSlash: false,
+      ignorePath: '/public/'
+    }))
+    .pipe(rename('index.tmpl'))
+    .pipe(gulp.dest('templates'));
+});
+
+gulp.task('build-dev', [
+  'build-html-dev'
+], function() {
+});
+
+gulp.task('default', function(){
+  return gutil.log("gulp is running")
+})
 
