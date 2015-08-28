edc4183dd9f2b7180a18449983462ce137f69104
Rama moorthy <rama.moorthy@razerzone.com>
2015-08-28 16:52:28 +0800

    add validation

--- backend/signup/signup.go
+++ backend/signup/signup.go
@@ -4,12 +4,24 @@ import (
 	"github.com/martini-contrib/binding"
 )
 
-type signupForm struct {
+type SignupForm struct {
   Name string `form:"name"` binding:"required"
   Email string `form:"email"` binding:"required"
   Role string `form:"role"` binding:"required"
-  Country `form:"email"` binding:"required"
-  Address `form:"email"`
-  ContactNo `form:"email"`
-  Notification `form:"email"`
+  Country `form:"country"` binding:"required"
+  Address `form:"address"`
+  ContactNo `form:"contactno"`
+  Notification `form:"notification"`
+}
+
+func (signupForm SignupForm) Validate(errors binding.Errors, req *http.Request) binding.Errors {
+
+    v := NewValidation(&errors, signupForm)
+
+    // //run some validators
+    v.Validate(&signupForm.Name).Key("name").MaxLength(200)
+    v.Validate(&signupForm.Email).Default("Custom Email Validation Message").Classify("email-class").Email()
+    v.Validate(&signupForm.Comments).TrimSpace().MinLength(10)
+
+    return *v.Errors.(*binding.Errors)
 }
